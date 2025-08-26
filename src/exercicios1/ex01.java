3515111d4fa04b37dcfd7cf3db2bcdbc543be7e8
Kaio Meireles <dickkaiosilva@gmail.com>
2025-08-25 21:46:03 -0300

    Exercicio 01

---
+++ src/exercicios1/ex01.java
@@ -0,0 +1,20 @@
+package exercicios1;
+
+import java.util.Scanner;
+
+public class ex01 {
+
+    public static void main(String[] args) {
+
+        //Linha abaixo representa o Scanf
+        Scanner ler = new Scanner(System.in);
+
+        System.out.println("Digite um numero inteiro: ");
+        int num = ler.nextInt();
+
+        System.out.println("O número escolhido foi: " + num);
+        System.out.println("Antecessor: " + (num-1));
+        System.out.println("Sucessor: " + (num+1));
+
+    }
+}
