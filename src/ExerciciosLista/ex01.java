6a94e0c60181a48883977936bbd168883a28f728
Kaio Meireles <dickkaiosilva@gmail.com>
2025-08-28 16:08:44 -0300

    Exercicio 02

--- src/ExerciciosLista/ex01.java
+++ src/ExerciciosLista/ex01.java
@@ -1,4 +1,4 @@
-package exercicios1;
+package ExerciciosLista;
 
 import java.util.Scanner;
 
---
+++ src/ExerciciosLista/ex02.java
@@ -0,0 +1,20 @@
+package ExerciciosLista;
+
+import java.util.Scanner;
+
+public class ex02 {
+    public static void main(String[] args) {
+
+        float distancia, gas, mediaGas;
+
+        Scanner ler = new Scanner(System.in);
+        System.out.printf("CÁLCULO DE GASTO DE COMBUSTÍVEL\n\n");
+        System.out.println("Qual foi a distancia percorrida? ");
+        distancia = ler.nextFloat();
+
+        System.out.println("Quantos litros de gasolina foram gastos ao todo? ");
+        gas = ler.nextFloat();
+        mediaGas = (distancia/gas);
+        System.out.printf("Seu veículo fez uma média de %.2f km/L\n",mediaGas);
+    }
+}
